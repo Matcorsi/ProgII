9830fa477f3b0e9a0ea1aacac0d4332da3e7106b
Matcorsi <Mateocorsidev@gmail.com>
2025-09-23 20:07:45 -0300

    Muebleria

--- Practico3/Ejercicio2/Mueble.java
+++ Practico3/Ejercicio2/Mueble.java
@@ -17,5 +17,55 @@ public class Mueble {
         this.color = color;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getCosto() {
+        return valor_costo;
+    }
+
+    public void setCosto(double valor_costo) {
+        this.valor_costo = valor_costo;
+    }
+
+    public double getValor_venta() {
+        return valor_venta;
+    }
+
+    public void setValor_venta(double valor_venta) {
+        this.valor_venta = valor_venta;
+    }
+
+    public String getMadera() {
+        return madera;
+    }
+
+    public void setMadera(String madera) {
+        this.madera = madera;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    
+
     
 }
--- Practico3/Ejercicio2/Muebleria.java
+++ Practico3/Ejercicio2/Muebleria.java
@@ -3,9 +3,24 @@ package Practico3.Ejercicio2;
 import java.util.ArrayList;
 
 public class Muebleria {
+        private double porcentajaVenta = 1.35;
         private ArrayList<Mueble> muebles_stock;
         
         public Muebleria(){
             this.muebles_stock = new ArrayList<>();
         }
+
+        public void addMueble(Mueble mueble){
+            this.muebles_stock.add(mueble);
+        }
+
+        public void listaValoresMuebles(){
+            
+        }
+
+        public double precioVenta(Mueble mueble){
+            return mueble.getValor_venta() * porcentajaVenta;
+        }
+
+
 }
