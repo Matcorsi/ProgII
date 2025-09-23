89f1a5e51f556ba3a129e3c3df8f6111f2b51d26
Matcorsi <Mateocorsidev@gmail.com>
2025-09-23 18:39:15 -0300

    Tp3_Ejer2

---
+++ Practico3/Ejercicio2/Mueble.java
@@ -0,0 +1,21 @@
+package Practico3.Ejercicio2;
+
+public class Mueble {
+    private String nombre;
+    private double peso;
+    private double valor_costo;
+    private double valor_venta;
+    public String madera;
+    public String color;
+
+    public Mueble(String nombre, double peso, double valor_costo, double valor_venta, String madera, String color){
+        this.nombre = nombre;
+        this.peso = peso;
+        this.valor_costo = valor_costo;
+        this.valor_venta = valor_venta;
+        this.madera = madera;
+        this.color = color;
+    }
+
+    
+}
---
+++ Practico3/Ejercicio2/Muebleria.java
@@ -0,0 +1,11 @@
+package Practico3.Ejercicio2;
+
+import java.util.ArrayList;
+
+public class Muebleria {
+        private ArrayList<Mueble> muebles_stock;
+        
+        public Muebleria(){
+            this.muebles_stock = new ArrayList<>();
+        }
+}
